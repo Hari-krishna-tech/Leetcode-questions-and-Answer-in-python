b9e1c47a89168e1215e2e9c9894422ae8a588b60
Hari-krishna-tech <jvenkadam@gmail.com>
2020-05-22 15:22:36 +0000

    satisfied math solution

--- a3-leetcode_question.py
+++ a3-leetcode_question.py
@@ -19,12 +19,8 @@ def Largest_palindrome_form_n_digit_no(n):
   for i in range(10**n,10**(n-1),-1):
     for j in range(10**n,10**(n-1),-1):
       if is_palindrome(i * j):
-        palindrome.append(i * j)
-        
+        palindrome.append(i * j)      
   return max(palindrome)%1337
-
-
-
 
 def is_palindrome(integer):
   str_int=str(integer)
@@ -33,3 +29,23 @@ def is_palindrome(integer):
 
 print (Largest_palindrome_form_n_digit_no(2))
 # good way
+"""
+Let Palindrome = X * Y, both have n digits, and assume they are very close to 10^n
+Denote X = 10^n - i, Y = 10^n - j, with assumption: i*j < 10^n
+Palindrome = upper*10^n + lower = (10^n - i)*(10^n - j) = (10^n-i-j)*10^n + i*j
+therefore: upper = 10^n-i-j, lower = i*j
+Let a = i + j, then lower = i*(a-i), upper = 10^n-a
+Algorithm: we iterate a and search for an integer i
+i^2 - a*i + lower = 0 => (i-a/2)^2 = 0.25*a^2 - lower
+Given a start from 2, check if sqrt(a^2 - lower*4) is an integer, then return upper*10^n + lower"""
+def largest_palindrome_product_of_n_digit_no(n):
+  if n == 1: return 9
+  a = 2
+  while a < 10**n:
+    upper = 10**n-a
+    lower = int(str(upper)[::-1])
+    if a**2-lower*4 >= 0 and (a**2-lower*4)**0.5 == int((a**2-lower*4)**0.5):
+      return (upper*10**n+lower) % 1337
+    a += 1
+
+print(largest_palindrome_product_of_n_digit_no(4))
